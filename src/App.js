e0d32c1f4c3542c1bb49b824367d8f386771402a
Sarkies <Ahmed.Sarkes@gmail.com>
2024-03-25 08:49:58 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React, { useEffect } from "react";
 import Cookies from "js-cookie";
+import { ProtectRoutes } from "./routes";
 /* eslint-disable no-unused-vars */
-import { Navigate, Route, Routes } from "react-router-dom";
 import locale from "./utils/locale";
-import {
-  LoginLayout,
-  Login,
-  Home,
-  Dashboard,
-  SubAdmins,
-  Users,
-  Messages,
-  Settings,
-  Slider,
-  TermsAndConditions,
-  CategoriesArticle,
-  CategoriesImage,
-  CategoriesAudio,
-  CategoriesBook,
-  SubCategoriesBook,
-  Elder,
-  Articles,
-  Audios,
-  Books,
-  Images,
-  CodeContent,
-} from "./components";
 
 const App = () => {
   const lng = Cookies.get("i18next") || "ar";
@@ -35,58 +12,10 @@ const App = () => {
     document.documentElement.lang = lng;
   }, [lng]);
 
-  return <div className="App">{auth ? <RoutesHome /> : <RoutesLogin />}</div>;
-};
-
-const RoutesLogin = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginLayout />}>
-        <Route
-          path=""
-          element={<Navigate to="/dr-omar/login" replace={true} />}
-        />
-        <Route
-          path="*"
-          element={<Navigate to="/dr-omar/login" replace={true} />}
-        />
-        <Route
-          path="dr-omar"
-          element={<Navigate to="/dr-omar/login" replace={true} />}
-        />
-        <Route path="dr-omar/login" element={<Login />} />
-      </Route>
-    </Routes>
-  );
-};
-
-const RoutesHome = () => {
-  return (
-    <Routes>
-      <Route path="/dr-omar" element={<Home />}>
-        <Route path="" element={<Navigate to="/dr-omar/dashboard" />} />
-        <Route path="login" element={<Navigate to="/dr-omar/dashboard" />} />
-        <Route path="*" element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="sub-admins" element={<SubAdmins />} />
-        <Route path="users" element={<Users />} />
-        <Route path="messages" element={<Messages />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="code-content" element={<CodeContent />} />
-        <Route path="slider" element={<Slider />} />
-        <Route path="terms&conditions" element={<TermsAndConditions />} />
-        <Route path="categories-article" element={<CategoriesArticle />} />
-        <Route path="categories-image" element={<CategoriesImage />} />
-        <Route path="categories-audio" element={<CategoriesAudio />} />
-        <Route path="main-categories-book" element={<CategoriesBook />} />
-        <Route path="sub-categories-book" element={<SubCategoriesBook />} />
-        <Route path="elder" element={<Elder />} />
-        <Route path="articles" element={<Articles />} />
-        <Route path="audios" element={<Audios />} />
-        <Route path="books" element={<Books />} />
-        <Route path="images" element={<Images />} />
-      </Route>
-    </Routes>
+    <div className="App">
+      <ProtectRoutes auth={auth} />
+    </div>
   );
 };
 
