2fac593aeefbf0579260e686950747a3b439ac64
Sarkies <Ahmed.Sarkes@gmail.com>
2024-04-15 17:48:41 +0200

    update Elder Component

--- src/components/Elders/Elder.jsx
+++ src/components/Elders/Elder.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Books, Articles, Audios } from '../'
+import React from "react";
+import { Books, Articles, Audios } from "../";
+import { useTranslation } from "react-i18next";
 const Elder = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-        <Books />
-        <Articles />
-        <Audios />
+      <div className="table-header justify-content-end pt-5 pe-5">
+        <h2>{t("elders.title")}</h2>
+      </div>
+      <Books />
+      <Articles />
+      <Audios />
     </div>
-  )
-}
+  );
+};
 
-export default Elder
+export default Elder;
