8e9bcde9eefb16fa27da6603e81a02970ecbb263
Sarkies <Ahmed.Sarkes@gmail.com>
2024-04-25 02:50:38 +0200

    Update NewUsers Component

--- src/components/NewUsers/NewUsers.jsx
+++ src/components/NewUsers/NewUsers.jsx
@@ -26,11 +26,31 @@ const NewUsers = () => {
       email: true,
       phone: true,
       created_at: true,
-      updated_at: true,
       email_verified_at: true,
       phone_verified_at: true,
+      subscription: true,
+      login_count: true,
+      register_method: true,
+      activation: true,
     },
   });
+
+  // Data
+  const data = newUsers?.map((user) => ({
+    ...user,
+    created_at: new Date(user.created_at).toLocaleDateString(),
+    updated_at: new Date(user.updated_at).toLocaleDateString(),
+    email_verified_at:
+      user.email_verified_at === null
+        ? t("newUsers.columns.notVerified")
+        : t("newUsers.columns.verified"),
+    phone_verified_at:
+      user.phone_verified_at === null
+        ? t("newUsers.columns.notVerified")
+        : t("newUsers.columns.verified"),
+    is_active: user.is_active === 1 ? t("active") : t("inactive"),
+    privacy: user.privacy === "public" ? t("public") : t("private"),
+  }));
 
   // Filtration, Sorting, Pagination
   // Columns
@@ -39,21 +59,35 @@ const NewUsers = () => {
     { id: 1, name: "name", label: t("newUsers.columns.name") },
     { id: 2, name: "email", label: t("newUsers.columns.email") },
     { id: 3, name: "phone", label: t("newUsers.columns.phone") },
-    { id: 4, name: "created_at", label: t("newUsers.columns.created_at") },
-    { id: 5, name: "updated_at", label: t("newUsers.columns.updated_at") },
+    { id: 4, name: "subscription", label: t("user.columns.subscription") },
+    { id: 5, name: "created_at", label: t("user.columns.created_at") },
+    { id: 6, name: "login_count", label: t("user.columns.login_count") },
     {
-      id: 6,
+      id: 7,
+      name: "register_method",
+      label: t("user.columns.register_method"),
+    },
+    {
+      id: 8,
       name: "email_verified_at",
       label: t("newUsers.columns.email_verified_at"),
     },
     {
-      id: 7,
+      id: 9,
       name: "phone_verified_at",
       label: t("newUsers.columns.phone_verified_at"),
     },
+
+    { id: 10, name: "activation", label: t("activation") },
   ];
-  const { PaginationUI, handleSort, searchResults } = useFiltration({
-    rowData: newUsers,
+  const {
+    PaginationUI,
+    handleSort,
+    searchResultsNewUsers,
+    handleToggleColumns,
+    handleSearch,
+  } = useFiltration({
+    rowData: data,
     toggle,
     setToggle,
   });
@@ -73,14 +107,77 @@ const NewUsers = () => {
         <h2>{t("newUsers.title")}</h2>
       </div>
       <div className="scholar">
+        <div className="table-header">
+          {/* Search */}
+          <div
+            className="search-container form-group-container form-input"
+            style={{
+              width: "65%",
+            }}
+          >
+            <input
+              type="text"
+              className="form-input"
+              placeholder={t("searchNewUser")}
+              value={toggle.searchTerm}
+              onChange={handleSearch}
+            />
+          </div>
+          {/* Show and Hide Columns */}
+          <div className="dropdown columns form-input">
+            <button
+              type="button"
+              onClick={() => {
+                setToggle({
+                  ...toggle,
+                  activeColumn: !toggle.activeColumn,
+                });
+              }}
+              className="dropdown-btn d-flex justify-content-between align-items-center"
+            >
+              <span>{t("columnsFilter")}</span>
+              <TiArrowSortedUp
+                className={`dropdown-icon ${
+                  toggle.activeColumn ? "active" : ""
+                }`}
+              />
+            </button>
+            <div
+              className={`dropdown-content ${
+                toggle.activeColumn ? "active" : ""
+              }`}
+              style={{
+                width: "180px",
+                maxHeight: "160px",
+              }}
+            >
+              {columns.map((column) => (
+                <button
+                  type="button"
+                  key={column.id}
+                  className={`item filter`}
+                  onClick={() => handleToggleColumns(column.name)}
+                >
+                  <span className="d-flex justify-content-start align-items-center gap-2">
+                    <input
+                      type="checkbox"
+                      className="checkbox-column"
+                      checked={toggle.toggleColumns[column.name]}
+                      readOnly
+                    />
+                    <span>{column.label}</span>
+                  </span>
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
         <table className="table-body">
           <thead>
             <tr>
-            {
-              toggle.toggleColumns?.id && (
-              <th className="table-th">{t("index")}</th>
-              )
-            }
+              {toggle.toggleColumns?.id && (
+                <th className="table-th">{t("index")}</th>
+              )}
               {toggle.toggleColumns?.name && (
                 <th className="table-th" onClick={() => handleSort(columns[1])}>
                   {t("newUsers.columns.name")}
@@ -117,9 +214,9 @@ const NewUsers = () => {
                   ) : null}
                 </th>
               )}
-              {toggle.toggleColumns?.created_at && (
+              {toggle.toggleColumns?.subscription && (
                 <th className="table-th" onClick={() => handleSort(columns[4])}>
-                  {t("newUsers.columns.created_at")}
+                  {t("user.columns.subscription")}
                   {toggle.sortColumn === columns[4].name ? (
                     toggle.sortOrder === "asc" ? (
                       <TiArrowSortedUp />
@@ -129,9 +226,9 @@ const NewUsers = () => {
                   ) : null}
                 </th>
               )}
-              {toggle.toggleColumns?.updated_at && (
+              {toggle.toggleColumns?.created_at && (
                 <th className="table-th" onClick={() => handleSort(columns[5])}>
-                  {t("newUsers.columns.updated_at")}
+                  {t("user.columns.created_at")}
                   {toggle.sortColumn === columns[5].name ? (
                     toggle.sortOrder === "asc" ? (
                       <TiArrowSortedUp />
@@ -141,9 +238,9 @@ const NewUsers = () => {
                   ) : null}
                 </th>
               )}
-              {toggle.toggleColumns?.email_verified_at && (
+              {toggle.toggleColumns?.login_count && (
                 <th className="table-th" onClick={() => handleSort(columns[6])}>
-                  {t("newUsers.columns.email_verified_at")}
+                  {t("user.columns.login_count")}
                   {toggle.sortColumn === columns[6].name ? (
                     toggle.sortOrder === "asc" ? (
                       <TiArrowSortedUp />
@@ -153,9 +250,9 @@ const NewUsers = () => {
                   ) : null}
                 </th>
               )}
-              {toggle.toggleColumns?.phone_verified_at && (
+              {toggle.toggleColumns?.register_method && (
                 <th className="table-th" onClick={() => handleSort(columns[7])}>
-                  {t("newUsers.columns.phone_verified_at")}
+                  {t("user.columns.register_method")}
                   {toggle.sortColumn === columns[7].name ? (
                     toggle.sortOrder === "asc" ? (
                       <TiArrowSortedUp />
@@ -164,6 +261,33 @@ const NewUsers = () => {
                     )
                   ) : null}
                 </th>
+              )}
+              {toggle.toggleColumns?.email_verified_at && (
+                <th className="table-th" onClick={() => handleSort(columns[8])}>
+                  {t("newUsers.columns.email_verified_at")}
+                  {toggle.sortColumn === columns[8].name ? (
+                    toggle.sortOrder === "asc" ? (
+                      <TiArrowSortedUp />
+                    ) : (
+                      <TiArrowSortedDown />
+                    )
+                  ) : null}
+                </th>
+              )}
+              {toggle.toggleColumns?.phone_verified_at && (
+                <th className="table-th" onClick={() => handleSort(columns[9])}>
+                  {t("newUsers.columns.phone_verified_at")}
+                  {toggle.sortColumn === columns[9].name ? (
+                    toggle.sortOrder === "asc" ? (
+                      <TiArrowSortedUp />
+                    ) : (
+                      <TiArrowSortedDown />
+                    )
+                  ) : null}
+                </th>
+              )}
+              {toggle.toggleColumns?.activation && (
+                <th className="table-th">{t("activation")}</th>
               )}
             </tr>
           </thead>
@@ -206,15 +330,17 @@ const NewUsers = () => {
             </tbody>
           )}
           {/* No Data */}
-          {searchResults?.length === 0 && error === null && !loading && (
-            <tbody>
-              <tr className="no-data-container">
-                <td className="table-td" colSpan="8">
-                  <p className="no-data mb-0">{t("noData")}</p>
-                </td>
-              </tr>
-            </tbody>
-          )}
+          {searchResultsNewUsers?.length === 0 &&
+            error === null &&
+            !loading && (
+              <tbody>
+                <tr className="no-data-container">
+                  <td className="table-td" colSpan="8">
+                    <p className="no-data mb-0">{t("noData")}</p>
+                  </td>
+                </tr>
+              </tbody>
+            )}
           {/* There is no any columns */}
           {Object.values(toggle.toggleColumns).every(
             (column) => column === false
@@ -228,109 +354,141 @@ const NewUsers = () => {
             </tbody>
           )}
           {/* Data */}
-          {searchResults?.length > 0 && error === null && loading === false && (
-            <tbody>
-              {searchResults?.map((result, idx) => (
-                <tr key={result?.id + new Date().getDate()}>
-                  {toggle.toggleColumns?.id && (
-                    <td className="table-td">{idx + 1}#</td>
-                  )}
-                  {
-                    toggle.toggleColumns?.name &&
-                  <td className="table-td name">{result?.name}</td>
-                  }
-                  {
-                    toggle.toggleColumns?.email &&
-                  <td className="table-td email">
-                    <a href={`mailto:${result?.email}`}>{result?.email}</a>
-                  </td>
-                  }
-                  {
-                    toggle.toggleColumns?.phone &&
-                  <td className="table-td phone">
-                    <a href={`mailto:${result?.phonenumber}`}>
-                      {result?.phonenumber}
-                    </a>
-                  </td>
-                  }
-                  {
-                    toggle.toggleColumns?.created_at &&
-                  <td className="table-td created_at">
-                    {new Date(result?.created_at).toLocaleDateString()}
-                  </td>
-                  }
-                  {
-                    toggle.toggleColumns?.updated_at &&
-                  <td className="table-td updated_at">
-                    {new Date(result?.updated_at).toLocaleDateString()}
-                  </td>
-                  }
-                  {
-                    toggle.toggleColumns?.email_verified_at &&
-                  <td className="table-td email_verified_at">
-                    {result?.email_verified_at === null ? (
-                      <span
-                        style={{
-                          color: "#fff",
-                          backgroundColor: "#dc3545",
-                          padding: "0.2rem 0.5rem",
-                          borderRadius: "0.2rem",
-                        }}
-                      >
-                        {t("newUsers.columns.notVerified")}
-                      </span>
-                    ) : (
-                      <span
-                        style={{
-                          color: "#fff",
-                          backgroundColor: "#28a745",
-                          padding: "0.2rem 0.5rem",
-                          borderRadius: "0.2rem",
-                        }}
-                      >
-                        {t("newUsers.columns.verified")}
-                      </span>
+          {searchResultsNewUsers?.length > 0 &&
+            error === null &&
+            loading === false && (
+              <tbody>
+                {searchResultsNewUsers?.map((result, idx) => (
+                  <tr key={result?.id + new Date().getDate()}>
+                    {toggle.toggleColumns?.id && (
+                      <td className="table-td">{idx + 1}#</td>
                     )}
-                  </td>
-                  }
-                  {
-                    toggle.toggleColumns?.phone_verified_at &&
-                  <td className="table-td phone_verified_at">
-                    {result?.phone_verified_at === null ? (
-                      <span
-                        style={{
-                          color: "#fff",
-                          backgroundColor: "#dc3545",
-                          padding: "0.2rem 0.5rem",
-                          borderRadius: "0.2rem",
-                        }}
-                      >
-                        {t("newUsers.columns.notVerified")}
-                      </span>
-                    ) : (
-                      <span
-                        style={{
-                          color: "#fff",
-                          backgroundColor: "#28a745",
-                          padding: "0.2rem 0.5rem",
-                          borderRadius: "0.2rem",
-                        }}
-                      >
-                        {t("newUsers.columns.verified")}
-                      </span>
+                    {toggle.toggleColumns?.name && (
+                      <td className="table-td name">{result?.name}</td>
                     )}
-                  </td>
-                  }
-                </tr>
-              ))}
-            </tbody>
-          )}
+                    {toggle.toggleColumns?.email && (
+                      <td className="table-td email">
+                        <a href={`mailto:${result?.email}`}>{result?.email}</a>
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.phone && (
+                      <td className="table-td phone">
+                        <a href={`mailto:${result?.phonenumber}`}>
+                          {result?.phonenumber}
+                        </a>
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.subscription && (
+                      <td className="table-td subscription">
+                        <span
+                          className={`status ${
+                            result?.privacy === t("private")
+                              ? "inactive"
+                              : "active"
+                          }`}
+                        >
+                          {result?.privacy}
+                        </span>
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.created_at && (
+                      <td className="table-td created_at">
+                        {new Date(result?.created_at).toLocaleDateString()}
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.login_count && (
+                      <td className="table-td login_count">
+                        {result?.login_count}
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.register_method && (
+                      <td className="table-td register_method">
+                        {result?.type}
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.email_verified_at && (
+                      <td className="table-td email_verified_at">
+                        {result?.email_verified_at === t("inactive") ? (
+                          <span
+                            style={{
+                              color: "#fff",
+                              backgroundColor: "#dc3545",
+                              padding: "0.2rem 0.5rem",
+                              borderRadius: "0.2rem",
+                              display: "inline-block",
+                            }}
+                          >
+                            {t("newUsers.columns.notVerified")}
+                          </span>
+                        ) : (
+                          <span
+                            style={{
+                              color: "#fff",
+                              backgroundColor: "#28a745",
+                              padding: "0.2rem 0.5rem",
+                              borderRadius: "0.2rem",
+                              display: "inline-block",
+                            }}
+                          >
+                            {t("newUsers.columns.verified")}
+                          </span>
+                        )}
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.phone_verified_at && (
+                      <td className="table-td phone_verified_at">
+                        {result?.phone_verified_at === t("inactive") ? (
+                          <span
+                            style={{
+                              color: "#fff",
+                              backgroundColor: "#dc3545",
+                              padding: "0.2rem 0.5rem",
+                              borderRadius: "0.2rem",
+                              display: "inline-block",
+                            }}
+                          >
+                            {t("newUsers.columns.notVerified")}
+                          </span>
+                        ) : (
+                          <span
+                            style={{
+                              color: "#fff",
+                              backgroundColor: "#28a745",
+                              padding: "0.2rem 0.5rem",
+                              borderRadius: "0.2rem",
+                              display: "inline-block",
+                            }}
+                          >
+                            {t("newUsers.columns.verified")}
+                          </span>
+                        )}
+                      </td>
+                    )}
+                    {toggle.toggleColumns?.activation && (
+                      <td className="table-td">
+                        <span
+                          className="table-status badge"
+                          style={{
+                            backgroundColor:
+                              result?.is_active === t("active")
+                                ? "green"
+                                : "red",
+                          }}
+                        >
+                          {result?.is_active}
+                        </span>
+                      </td>
+                    )}
+                  </tr>
+                ))}
+              </tbody>
+            )}
         </table>
       </div>
       {/* Pagination */}
-      {searchResults?.length > 0 && error === null && loading === false && (
-        <PaginationUI />
-      )}
+      {searchResultsNewUsers?.length > 0 &&
+        error === null &&
+        loading === false && <PaginationUI />}
     </div>
   );
 };
