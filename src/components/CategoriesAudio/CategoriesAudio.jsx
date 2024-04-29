d0756495edef7eb1c71c83cdceabeabda311240c
Sarkies <Ahmed.Sarkes@gmail.com>
2024-04-30 00:02:27 +0300

    Update CategoriesAudio Component

--- src/components/CategoriesAudio/CategoriesAudio.jsx
+++ src/components/CategoriesAudio/CategoriesAudio.jsx
@@ -132,10 +132,10 @@ const CategoriesAudio = () => {
         if (result.isConfirmed) {
           dispatch(deleteAudioCategoryApi(audioCategory?.id)).then((res) => {
             if (!res.error) {
-              if (searchResults.length === 1) {
+              if (toggle.currentPage > 1 && searchResults.length === 1) {
                 setToggle({
                   ...toggle,
-                  currentPage: 1,
+                  currentPage: toggle.currentPage - 1,
                 });
               }
               dispatch(getAudiosCategoriesApi());
