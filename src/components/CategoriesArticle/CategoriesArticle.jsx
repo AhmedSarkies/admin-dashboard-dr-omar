6e60a03cc9fcd69ec9d72893d8d1a74d6d93c03e
Sarkies <Ahmed.Sarkes@gmail.com>
2024-04-30 00:02:37 +0300

    Update CategoriesArticle Component

--- src/components/CategoriesArticle/CategoriesArticle.jsx
+++ src/components/CategoriesArticle/CategoriesArticle.jsx
@@ -131,10 +131,10 @@ const CategoriesArticle = () => {
           dispatch(deleteArticleCategoryApi(articleCategory?.id)).then(
             (res) => {
               if (!res.error) {
-                if (searchResults.length === 1) {
+                if (toggle.currentPage > 1 && searchResults.length === 1) {
                   setToggle({
                     ...toggle,
-                    currentPage: 1,
+                    currentPage: toggle.currentPage - 1,
                   });
                 }
                 dispatch(getArticlesCategoriesApi());
