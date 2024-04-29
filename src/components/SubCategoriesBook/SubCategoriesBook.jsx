326ed00951406e575c2695422044a7fb78eb87f6
Sarkies <Ahmed.Sarkes@gmail.com>
2024-04-30 00:07:10 +0300

    Update SubCategoriesBook Component

--- src/components/SubCategoriesBook/SubCategoriesBook.jsx
+++ src/components/SubCategoriesBook/SubCategoriesBook.jsx
@@ -149,10 +149,10 @@ const SubCategoriesBook = () => {
           dispatch(deleteBookSubCategoryApi(bookSubCategory?.id)).then(
             (res) => {
               if (!res.error) {
-                if (searchResults.length === 1) {
+                if (toggle.currentPage > 1 && searchResults.length === 1) {
                   setToggle({
                     ...toggle,
-                    currentPage: 1,
+                    currentPage: toggle.currentPage - 1,
                   });
                 }
                 dispatch(getBooksSubCategoriesApi());
