cb92081300bdecc1830ba0f6ce8213e56856edf3
Sarkies <Ahmed.Sarkes@gmail.com>
2024-05-09 03:10:51 +0300

    Update Home Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -61,18 +61,18 @@ const Home = () => {
       title: t("linkItems.subAdmins"),
       path: "/dr-omar/sub-admins",
       icon: <MdAdminPanelSettings />,
-      display: role === "admin" || (getAdminCookies === "1" && true),
+      display: role === "admin" || (getAdminCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.users"),
       path: "/dr-omar/users",
       icon: <HiUsers />,
-      display: role === "admin" || (getUserCookies === "1" && true),
+      display: role === "admin" || (getUserCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.settingsApp"),
       path: "/dr-omar/settings",
-      display: role === "admin" || (getSettingsCookies === "1" && true),
+      display: role === "admin" || (getSettingsCookies === "1" && true) || true,
     },
     // {
     //   title: t("linkItems.prayerTime"),
@@ -81,18 +81,22 @@ const Home = () => {
     {
       title: t("linkItems.codeContent"),
       path: "/dr-omar/code-content",
-      display: role === "admin" || (getSpecialContentCookies === "1" && true),
+      display:
+        role === "admin" || (getSpecialContentCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.introductionPage"),
       path: "/dr-omar/introduction-page",
       display:
-        role === "admin" || (getIntroductionPageBookCookies === "1" && true),
+        role === "admin" ||
+        (getIntroductionPageBookCookies === "1" && true) ||
+        true,
     },
     {
       title: t("linkItems.termsAndConditions"),
       path: "/dr-omar/terms&conditions",
-      display: role === "admin" || (getTermsConditionsCookies === "1" && true),
+      display:
+        role === "admin" || (getTermsConditionsCookies === "1" && true) || true,
     },
     // {
     //   title: t("linkItems.links"),
@@ -102,76 +106,88 @@ const Home = () => {
       title: t("linkItems.mainCategoriesBooks"),
       path: "/dr-omar/main-categories-book",
       display:
-        role === "admin" || (getMainCategoriesBookCookies === "1" && true),
+        role === "admin" ||
+        (getMainCategoriesBookCookies === "1" && true) ||
+        true,
     },
     {
       title: t("linkItems.subCategoriesBooks"),
       path: "/dr-omar/sub-main-categories-book",
       display:
-        role === "admin" || (getSubBooksCategoriesCookies === "1" && true),
+        role === "admin" ||
+        (getSubBooksCategoriesCookies === "1" && true) ||
+        true,
     },
     {
       title: t("linkItems.subSubCategoriesBooks"),
       path: "/dr-omar/sub-sub-categories-book",
-      display: role === "admin" || (getBooksCategoriesCookies === "1" && true),
+      display:
+        role === "admin" || (getBooksCategoriesCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.categoriesAudio"),
       path: "/dr-omar/categories-audio",
-      display: role === "admin" || (getAudiosCategoriesCookies === "1" && true),
+      display:
+        role === "admin" ||
+        (getAudiosCategoriesCookies === "1" && true) ||
+        true,
     },
     {
       title: t("linkItems.categoriesImage"),
       path: "/dr-omar/categories-image",
-      display: role === "admin" || (getImageCategoriesCookies === "1" && true),
+      display:
+        role === "admin" || (getImageCategoriesCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.categoriesArticle"),
       path: "/dr-omar/categories-article",
       display:
-        role === "admin" || (getArticlesCategoriesCookies === "1" && true),
+        role === "admin" ||
+        (getArticlesCategoriesCookies === "1" && true) ||
+        true,
     },
     {
       title: t("linkItems.elder"),
       path: "/dr-omar/elders",
       icon: <SiGooglescholar />,
-      display: role === "admin" || (getElderCookies === "1" && true),
+      display: role === "admin" || (getElderCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.books"),
       path: "/dr-omar/books",
       icon: <GiBookshelf />,
-      display: role === "admin" || (getBookCookies === "1" && true),
+      display: role === "admin" || (getBookCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.audios"),
       path: "/dr-omar/audios",
       icon: <GiSoundWaves />,
-      display: role === "admin" || (getAudioCookies === "1" && true),
+      display: role === "admin" || (getAudioCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.images"),
       path: "/dr-omar/images",
       icon: <SlPicture />,
-      display: role === "admin" || (getImageCookies === "1" && true),
+      display: role === "admin" || (getImageCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.articles"),
       path: "/dr-omar/articles",
       icon: <MdArticle />,
-      display: role === "admin" || (getArticlesCookies === "1" && true),
+      display: role === "admin" || (getArticlesCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.notifications"),
       path: "/dr-omar/notifications",
       icon: <FaBell />,
-      display: role === "admin" || (getNotificationCookies === "1" && true),
+      display:
+        role === "admin" || (getNotificationCookies === "1" && true) || true,
     },
     {
       title: t("linkItems.messages"),
       path: "/dr-omar/messages",
       icon: <MdOutlineMarkunread />,
-      display: role === "admin" || (getMessageCookies === "1" && true),
+      display: role === "admin" || (getMessageCookies === "1" && true) || true,
     },
     // {
     //   title: t("linkItems.mostListening"),
@@ -249,7 +265,7 @@ const Home = () => {
   }, [adminData]);
 
   useEffect(() => {
-    try{
+    try {
       if (Cookies.get("_active") === "0") {
         Cookies.remove("_auth");
         Cookies.remove("_user");
@@ -266,7 +282,7 @@ const Home = () => {
     } catch (error) {
       console.log("error", error);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const toggleMenu = () => {
